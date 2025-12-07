5c627305734e88596ab3bfa696f35e4aef305e89
rnozila2197 <yjamshid2197@gmail.com>
2025-12-07 14:53:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# 1
+# a = int(input("Xarorat kiriting: "))
+# 
+# if a > 50:
+#     print("Juda issiq uyda qoling!")
+# elif 20 and 30:
+#     print("Havo yahshi sayr qling!")
+# elif 10 and 20:
+#     print("Havo salqin koylak kiying!")
+# elif a < 0:
+#     print("Juda sovuq, ehtiyot boling!")
+
+# 2
+# x = int(input("Ovqat turini tanlang: "))
+# 
+# p = "Pizza"
+# b = "Burger"
+# s = "Salat"
+# 
+# if p == 0:
+#     print("Pizzani narxi: 50.000")
+# elif b == 0:
+#     print("Burgerni narxi: 30.000")
+# elif s == 0:
+#     print("Salat narxi: 20.000")
+# else:
+#     print("Bizda bunday taom yoq!")
+
+# 3
+# a = int(input("Yosh kiriting: "))
+# 
+# if a <= 18:
+#     print("Sizga 50% chegirma bor!")
+# elif 18 and 60:
+#     print("Chegirma yoq!")
+# elif a >= 60:
+#     print("Keksalarga 30% chegirma bor!")
